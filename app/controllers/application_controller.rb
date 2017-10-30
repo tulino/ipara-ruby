75c249c7a80270cd4e28fb5e57afc059d9086d95
Fatih Coşkun <fatih@codevist.com>
2017-10-30 17:57:27 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     @@settings=Settings.new 
     @@settings.PublicKey = "" #//"Public Magaza Anahtarı - size mağaza başvurunuz sonucunda gönderilen publik key (açık anahtar) bilgisini kullanınız.",
     @@settings.PrivateKey = "" #"Private Magaza Anahtarı  - size mağaza başvurunuz sonucunda gönderilen privaye key (gizli anahtar) bilgisini kullanınız.",
-    @@settings.BaseUrl = "https://apitest.ipara.com/" #iPara web servisleri API url'lerinin başlangıç bilgisidir. Restful web servis isteklerini takip eden kodlar halinde bulacaksınız.
+    @@settings.BaseUrl = "https://api.ipara.com/" #iPara web servisleri API url'lerinin başlangıç bilgisidir. Restful web servis isteklerini takip eden kodlar halinde bulacaksınız.
                                         # Örneğin "https://api.ipara.com/" + "/rest/payment/auth"  = "https://api.ipara.com/rest/payment/auth" 
     @@settings.Version = "1.0" # Kullandığınız iPara API versiyonudur. 
     @@settings.Mode = "T" #Test -> T, entegrasyon testlerinin sırasında "T" modunu, canlı sisteme entegre olarak ödeme almaya başlamak için ise Prod -> "P" modunu kullanınız.
